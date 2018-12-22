d9f3b56adc72c3e314b892ce9d6a2f8d60f5d9a8
kostafey <kostafey@gmail.com>
2018-12-22 21:25:39 +0300

    Add leiningen project.clj

---
+++ project.clj
@@ -0,0 +1,9 @@
+(defproject org.clojars.kostafey/java.jdbc "0.7.9-SNAPSHOT"
+  :description "JDBC from Clojure (formerly clojure.contrib.sql)"
+  :url "https://github.com/clojure/java.jdbc"
+  :license {:name "Eclipse Public License 1.0"
+            :url "https://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :source-paths ["src/main/clojure"]
+  :deploy-repositories [["releases" :clojars]
+                        ["snapshots" :clojars]])
